42fb603c9aeeecab9ea846a020dcace853dfd889
Abdullah Al Noman <nmnaba14@gmail.com>
2021-08-30 14:33:53 +0600

    function component and class component done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 // const name = 'React';
 // const element = React.createElement('h1', null, 'Hello React');
 // const element = <h1>Hello {name}</h1>;
@@ -12,13 +12,37 @@ import reactDom from 'react-dom';
 // }
 // console.log(getGrettings('Noman'));
 
-setInterval(() => {
-    const element = (
-        <div className="name">
-            <span className="span">Hello {new Date().toLocaleTimeString()}</span>
-            <img src="" alt="" />
-        </div>
-    );
-    reactDom.render(element, document.getElementById('root'));
-}, 1000);
+// setInterval(() => {
+//     const element = (
+//         <div className="name">
+//             <span className="span">Hello {new Date().toLocaleTimeString()}</span>
+//             <img src="" alt="" />
+//         </div>
+//     );
+//     reactDom.render(element, document.getElementById('root'));
+// }, 1000);
 // console.log(element);
+
+// react component
+// functional component
+// here Clock function is a component, locale is props & clock funtion's retun value is an element
+// function Clock({ locale }) {
+//     return (
+//         <div className="name">
+//             <span className="span">Hello {new Date().toLocaleTimeString(locale)}</span>
+//             <img src="" alt="" />
+//         </div>
+//     );
+// }
+
+// ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
+
+// example of a class component
+
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+ReactDOM.render(<Welcome name="Noman" />, document.getElementById('root'));
